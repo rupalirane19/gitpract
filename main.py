f7b6047e41b5ef938787f472562667960e9d07f9
rupalivasantrane109@gmail.com <rupalivasantrane109@gmail.com>
2022-05-19 11:21:00 +0530

    initial pgm

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas
+from sqlalchemy import create_engine
+
+path = r'C:\Users\USER\Desktop\studinfo.xlsx'
+df = pandas.read_excel( path )
+print(df)
+con = 'postgresql://postgres:pass123@localhost/datapract'
+db = create_engine (con )
+con1 = db.connect( )
+try:
+    df.to_sql ( 'stud1' , con1 , if_exists = 'append' , index = False , schema = 'test' )
+    print("data in")
+except Exception as e :
+    print(e)
+#assign
